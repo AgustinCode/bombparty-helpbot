7d07b00eecad0af4659926c193d3d304102cff28
justbrowsing37 <164421533+justbrowsing37@users.noreply.github.com>
2024-07-08 09:23:59 +0800

    Re-organize imports

--- driver.py
+++ driver.py
@@ -1,14 +1,16 @@
 import time
+import re
+import os
+import logging
+import random
+
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import re
-import os
-import logging
-import random
 
 class JKLMBot:
 
